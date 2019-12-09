7be026600eb09ae130572cb4446af63ac5c114ee
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2019-12-09 17:24:27 -0500

    Add structure for tests

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Exolnet\Test\Tests\Unit;
+
+class ExampleTest extends UnitTest
+{
+    /**
+     * @return void
+     */
+    public function testBasic(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/UnitTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Exolnet\Test\Tests\Unit;
+
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+abstract class UnitTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+}
